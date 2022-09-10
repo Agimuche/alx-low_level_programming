5617268fe6bca71fb96538c27b847cc2553de6b3
Agim <agimuche1@gmail.com>
2022-09-10 08:35:36 -0700

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,2 @@
-#!/bin/bash
-#include <studio.h>
-
-/**
-*main -print the string in the put function
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle"
-Return: 0
-*/
-int main(void)
-}
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-}
+#!/bin/Bash
+printf < "Programming is like building a multilingual puzzle">
