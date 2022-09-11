f384d59b7ee63603971b3170fa9d74900bcb9b84
Agim <agimuche1@gmail.com>
2022-09-11 06:44:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 /**
+ * manin - print the string in the put function
  *
- * * main - Prints a text
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+ * Descriptio: using the main function
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ * /
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	(printf"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
-}
+{
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,33 +2,38 @@
 
 /**
  *
- * * main - print the string in the put function
+ * * main - Prints the sizeof many var types
  *
  * *
  *
- * * Description: using the main function
- *
- * * this program prints "Prints the sizes of many var types"
- *
- * * Return: 0
+ * * Return: Always 0 (Success)
  *
  * */
+
 int main(void)
 
 {
 
-	char c;
-	int i;
-	long li;
-	long long lli;	
-	float f;
+	char a;
 
+	int b;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li))			        
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));			
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));				
+	long int c;
+
+	long long int d;
+
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
 	return (0);
 
 }
