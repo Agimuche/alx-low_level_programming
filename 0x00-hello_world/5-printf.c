cd4cb16dd668009f36cf742a79fd246f28a7b32d
Agim <agimuche1@gmail.com>
2022-09-11 07:05:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,16 +2,15 @@
 
 /**
  *
- * * main - Prints a text
+ * * main - prints a text
  *
  * *
  *
  * * Return: Always 0 (Success)
  *
  * */
-int main(void)
-
+int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+{
