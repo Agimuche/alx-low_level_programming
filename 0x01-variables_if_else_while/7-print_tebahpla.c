8c006ad5e88ec6b55e813500b497c2b18f7eaaaa
Agim <agimuche1@gmail.com>
2022-09-12 17:30:55 -0700

    all

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,23 +8,19 @@
  *
  ** Description: using the main function
  *
- ** this program prints "Programming is positive, zero, or negative
+ ** this program prints the lowercase alphabet in reverse, followed by a new line
  *
  ** Return: 0
  *
  **/
-
 int main(void)
-
 {
 	char c;
 
 	for (c = 'z'; c >= 'a'; c--)
 
 	{
-
 		putchar(c);
-
 	}
 
 	putchar('\n');
