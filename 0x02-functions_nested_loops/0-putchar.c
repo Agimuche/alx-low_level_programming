1da9b4322ed3c39dc2318bb7d590190e233d7e98
Agim <agimuche1@gmail.com>
2022-09-13 19:19:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,11 @@
 
 /**
  *
- ** main - print putchar
+ * main - print putchar
  *
- ** Return: always 0
+ * Return: always 0
  *
  **/
-
 int main(void)
 {
 	_putchar('_');
@@ -19,5 +18,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-											return (0);
+
+	return (0);
 }
