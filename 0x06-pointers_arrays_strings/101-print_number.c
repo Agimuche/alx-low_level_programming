01328032140e003481bd991ba51332b32e1ebd53
Agim <agimuche1@gmail.com>
2022-09-21 20:58:20 -0700

    all

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,21 @@
-#include "main.h"
-
-/**
- * set_string - Entry point
- * @s: input
- * @to: input
- * Return: Always 0 (Success)
- */
-void set_string(char **s, char *to)
-{
-	*s = to;
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * print_number - Prints an integer.                                                                                                  
+ * @n: The integer to be printed.                                                                                                     
+ */                                                                                                                                   
+void print_number(int n)                                                                                                              
+{                                                                                                                                     
+        unsigned int num = n;                                                                                                         
+                                                                                                                                      
+        if (n < 0)                                                                                                                    
+        {                                                                                                                             
+                _putchar('-');                                                                                                        
+                num = -num;                                                                                                           
+        }                                                                                                                             
+                                                                                                                                      
+        if ((num / 10) > 0)                                                                                                           
+                print_number(num / 10);                                                                                               
+                                                                                                                                      
+        _putchar((num % 10) + '0');                                                                                                   
 }
